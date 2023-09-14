4212d858fd4ad80b1923161cf9afde2f638acd28
GitForceJedi <Kencraw4ord@outlook.com>
2023-09-14 07:20:17 -0400

    added .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,13 +7,17 @@ import { ProductsModule } from './product/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './product/product.entity';
 
+console.log(process.env.DATABASE_NAME);
+console.log(process.env.DATABASE_USER);
+console.log(process.env.DATABASE_PASSWORD);
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      database: 'react_db1',
-      username: 'root',
-      password: 'sql9951',
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
       entities: [Product],
       synchronize: true,
     }),
